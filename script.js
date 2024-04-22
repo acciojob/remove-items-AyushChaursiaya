87eea01a23e172abee3ba6bd9ede6c780b66f4c4
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-23 00:02:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+function removeSelectedColor() {
+	var selectElement = document.getElementById("colorSelect");
+
+	var selectedOption = selectColor.options[selectElement.selectedIndex]
+	if(selectedOption) {
+		selectElement.removeChild(selectedOption )
+	}
+} 
+
+// function removeSelectedColor() {
+  var selectElement = document.getElementById("colorSelect");
+  var selectedOption = selectElement.options[selectElement.selectedIndex];
+  
+  if (selectedOption) {
+    selectElement.removeChild(selectedOption);
+  }
+}
